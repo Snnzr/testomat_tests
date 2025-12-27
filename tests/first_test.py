4d9bab35041be2138920d6ca9904fd55f29aa955
Nazar Snihur <snihurnazar@gmail.com>
2025-12-27 15:27:55 +0200

    MNFCT-2-module implemented changes to first test with kiss, dry, yagni principles

--- tests/first_test.py
+++ tests/first_test.py
@@ -8,16 +8,58 @@ def test_open_home_page(page: Page):
     expect(page.locator("[href*='sign_in'].login-item")).to_be_visible()
     expect(page.get_by_text("Log in", exact=True)).to_be_visible()
 
-def login_with_invalid_creds(page: Page):
-    page.goto("https://testomat.io")
+
+def test_login_with_invalid_creds(page: Page):
+    open_home_page(page)
+
     expect(page.locator("[href*='sign_in'].login-item")).to_be_visible()
+
     page.get_by_text("Log in", exact=True).click()
 
-    page.locator("#content-desktop #user_email").fill("newromka@gmail.ccom")
-    page.locator("#content-desktop #user_password").fill("sldsldsdssd")
-    page.get_by_role("button", name="Sign in").click()
+    login_user(page, email="EMAIL", password="WRONG_PASSWORD")
 
 
     expect(page.locator("#content-desktop").get_by_text("Invalid Email or password.")).to_be_visible()
     expect(page.locator("#content-desktop .common-flash-info")).to_have_text("Invalid Email or password.")
 
+
+def test_search_project_in_company(page: Page):
+    page.goto("https://app.testomat.io/users/sign_in")
+
+    login_user(page, email="EMAIL", password="CORRECT_PASSWORD")
+
+    target_project = "python manufacture"
+
+    search_for_project(page, target_project)
+
+    expect(page.get_by_role("heading", name=target_project)).to_be_visible()
+
+    expect(page.locator("ul li h3", has_text=target_project)).to_be_visible()
+
+
+def test_should_be_possible_to_open_free_project(page: Page):
+    page.goto("https://app.testomat.io/users/sign_in")
+    login_user(page, email="EMAIL", password="CORRECT_PASSWORD")
+    page.locator("#company_id").click()
+    page.locator("#company_id").select_option("Free Projects")
+
+    target_project = "python manufacture"
+    search_for_project(page, target_project)
+    expect(page.get_by_role("heading", name=target_project)).to_be_hidden()
+
+    expect(page.get_by_text("You have not created any projects yet")).to_be_visible(timeout=10000)
+
+
+def search_for_project(page: Page, target_project: str):
+    expect(page.get_by_role("searchbox", name="Search")).to_be_visible()
+    page.locator("#content-desktop #search").fill(target_project)
+
+
+def open_home_page(page: Page):
+    page.goto("https://testomat.io")
+
+
+def login_user(page: Page, email: str, password: str):
+    page.locator("#content-desktop #user_email").fill(email)
+    page.locator("#content-desktop #user_password").fill(password)
+    page.get_by_role("button", name="Sign in").click()
